6f7d294260fb9e10a91ce97adfd79b121cb35425
chaotix-nld <189079957+chaotix-nld@users.noreply.github.com>
2024-12-14 18:04:58 +0100

    created computer choices added random integer creator added if else statement to print computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+let choice1 = "rock";
+let choice2 = "Paper";
+let choice3 = "Scissors";
+let getComputerChoice = getRandomInt(3);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max + 1);
+}
+console.log(getComputerChoice);
+
+if (getComputerChoice === 1){
+    console.log(choice1);
+}
+else if (getComputerChoice === 2){
+    console.log(choice2);
+}
+else if (getComputerChoice === 3){
+    console.log(choice3);
+}
