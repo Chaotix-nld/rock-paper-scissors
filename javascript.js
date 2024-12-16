9262ff1daae528b94a81537d39cd1ff23aebb962
chaotix-nld <189079957+chaotix-nld@users.noreply.github.com>
2024-12-16 16:45:21 +0100

    created entire code for the game added comments to make code more readable struggling with updating the inputs per iteration in the loop finally got it to work // line 77/83

--- javascript.js
+++ javascript.js
@@ -1,35 +1,103 @@
-let choice1 = "rock";
-let choice2 = "Paper";
-let choice3 = "Scissors";
-let getComputerChoice = getRandomInt(3);
-let getHumanChoice = parseInt(prompt("Please enter your choice: 1 for Rock, 2 for Paper, 3 for Scissors: "));
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice(3);
 let humanScore = 0;
 let computerScore = 0;
 
-if (getHumanChoice === 1){
-    console.log("player picked " + choice1);
-}
-else if (getHumanChoice === 2){
-    console.log("player picked " + choice2);
-}
-else if (getHumanChoice === 3){
-    console.log("player picked" + choice3);
-}
-else{
-    console.log("you made a choice that doenst exsist, please try again");
+//gettig user input
+function getHumanChoice(){
+    return parseInt(prompt("please pick 1 for rock, 2 for paper, 3 for scissors", ''));
 }
 
-function getRandomInt(max) {
+//generate random number for computer choice
+function getComputerChoice(max){
     return Math.floor(Math.random() * max + 1);
-}
+}  
 
 
-if (getComputerChoice === 1){
-    console.log("computer picked " + choice1);
+//display human input
+function displayHumanInput(){
+    if (humanChoice === 1){
+        console.log("player picked Rock");
+    }
+    else if (humanChoice === 2){
+        console.log("player picked Paper");
+    }
+    else if (humanChoice === 3){
+        console.log("Player picked Scissors");
+    }
+    else {
+        console.log("Player made an invalid input");
+    }
+    }
+
+// display computer choice
+function displayComputerInput(){
+    if (computerChoice === 1){
+        console.log("computer picked Rock");
+    }
+    else if (computerChoice === 2){
+        console.log("computer picked Paper");
+    }
+    else if (computerChoice === 3){
+        console.log("computer picked Scissors");
+    }
+    }
+
+//calculate score
+function calculateScore(){
+    if (computerChoice === humanChoice){
+        console.log("Its a tie!");
+    }
+    else if (computerChoice === 1 && humanChoice === 2){
+        console.log("Player Won!")
+        humanScore ++;
+    }
+    else if (computerChoice === 1 && humanChoice === 3){
+        console.log("Computer Won!")
+        computerScore ++;
+    }
+    else if (computerChoice === 2 && humanChoice === 1){
+        console.log("Computer Won!")
+        computerScore ++;
+    }
+    else if (computerChoice === 2 && humanChoice === 3){
+        console.log("Player Won!")
+        humanScore ++;
+    }
+    else if (computerChoice === 3 && humanChoice === 1){
+        console.log("Player Won!")
+        humanScore ++;
+    }
+    else if (computerChoice === 3 && humanChoice === 2){
+        console.log("Computer Won!")
+        computerScore ++;
+    }
+}    
+//play 1 round
+function playRound(){
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice(3);
+    displayHumanInput();
+    displayComputerInput();
+    calculateScore();
+    
 }
-else if (getComputerChoice === 2){
-    console.log("computer picked " + choice2);
+
+//play 5 rounds
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        playRound()
+        console.log("player-score: " + humanScore + "; computer-score: " +computerScore);
+    }
+    if (humanScore > computerScore){
+        console.log("You have won the Game!!");
+    }
+    else if (humanScore < computerScore){
+        console.log("To bad, the computer won...");
+    }
+    else {
+        console.log("its a Tie!!!");
+    }
 }
-else if (getComputerChoice === 3){
-    console.log("computer picked " + choice3);
-}
+playGame();    
+
